7e589a004039528c0068fb0a006d2ab52d77dafe
Derrick Fink <derrickfink@boisestate.edu>
2017-07-24 14:07:51 -0600

    got deployment working on docker

---
+++ NotHotDog/dockerDeploy.ps1
@@ -0,0 +1,5 @@
+﻿dotnet publish -c Release
+cd ./bin/release/netcoreapp1.1/publish
+docker build -t nothotdog .
+docker tag nothotdog registry.heroku.com/nothotdog-dotnet/web
+docker push registry.heroku.com/nothotdog-dotnet/web
