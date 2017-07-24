5e4ba9d682d7403072c1790fc3616a717a2ffeaa
Derrick Fink <derrickfink@boisestate.edu>
2017-07-24 14:50:53 -0600

    reminder

--- NotHotDog/dockerDeploy.ps1
+++ NotHotDog/dockerDeploy.ps1
@@ -1,4 +1,5 @@
-﻿dotnet publish -c Release
+﻿# https://artdrozdov.blogspot.com/2017/06/deploying-net-core-web-app-with-docker.html
+dotnet publish -c Release
 cd ./bin/release/netcoreapp1.1/publish
 docker build -t nothotdog .
 docker tag nothotdog registry.heroku.com/nothotdog-dotnet/web
